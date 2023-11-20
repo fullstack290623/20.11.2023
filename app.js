ad3f96aa129380e345189158a5899b4f4765e37f
fullstack290623 <fullstack290623@gmail.com>
2023-11-20 21:29:13 +0200

    .

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 const dal = require('./dal')
 
 const express = require('express')
@@ -9,17 +8,14 @@ const port = 3000
 app.use(body_parser.json())
 
 // GET 
-app.get('/api/employees', async (request, response) => {
-    //console.log(request);
+app.get('/api/employee', async (request, response) => {
     const employees = await dal.get_all()
-    console.log('someone did a get for /api/users URL');
-    //response.send('<h1 style="color:blue">hi</h1>')
     response.json(employees)
 })
 // GET by ID
-app.get('/api/employee/:id', (request, response) => {
+app.get('/api/employee/:id', async (request, response) => {
     const user_id = parseInt(request.params.id)
-    const user = crud.get_by_id(user_id)
+    const user = await dal.get_by_id(user_id)
     if (user) {
         response.json(user)
     }
@@ -28,59 +24,48 @@ app.get('/api/employee/:id', (request, response) => {
     }
 })
 // POST
-app.post('/api/employee', (request, response) => {
+app.post('/api/employee', async (request, response) => {
     const new_user = request.body
-    const updated_user = crud.post(new_user)
-    response.status(201).json(updated_user)
-
+    const result = await dal.new_employee(new_user)
+    response.status(201).json(result)
 })
 // PUT
-app.put('/api/employee/:id', (request, response) => {
-    const updated_user_req = request.body
+app.put('/api/employee/:id', async (request, response) => {
     const user_id = parseInt(request.params.id)
-    const updated_or_created_user = crud.put(user_id, updated_user_req)
-    response.json(updated_or_created_user)
-})
-// PATCH
-app.patch('/api/employee/:id', (request, response) => {
-    const updated_user_req = request.body
-    const user_id = parseInt(request.params.id)
-    const updated_user = crud.patch(user_id, updated_user_req)
-    if (updated_user) {
-        response.json(updated_user)
+    const user = await dal.get_by_id(user_id)
+    if (user) {
+        // user exists ==> perform update
+        const updated_user_req = request.body
+        const result = await dal.update_emplyee(user_id, updated_user_req)
+        response.json(updated_user_req)
     }
     else {
-        response.status(404).json({ "error": `cannot find user with id ${user_id}` })
+        // user does NOT exist ==> perform insert
+        const new_user = request.body
+        const result = await dal.new_employee(new_user)
+        response.status(201).json(result)
     }
+})
+// PATCH
+app.patch('/api/employee/:id', async (request, response) => {
+    const updated_user_req = request.body
+    const user_id = parseInt(request.params.id)
+    const user = await dal.get_by_id(user_id)
+    // override only existing fields in the user from the db
+    const result = await dal.update_emplyee(user_id, { ...user, ...updated_user_req })
+    response.json({ result })
+
 })
 
 // DELETE
-app.delete('/api/employee/:id', (request, response) => {
+app.delete('/api/employee/:id', async (request, response) => {
     const user_id = parseInt(request.params.id)
-    const deleted = crud.delete_by_id(user_id)
-    if (deleted) {
-        response.status(204).json({ "status": `user with id = ${user_id} deleted` })
-    }
-    else {
-        response.status(404).json({ "error": `cannot find user with id ${user_id}` })
-    }
+    const result = await dal.delete_employee(user_id)
+    response.status(204).json({ result })
+
 })
 
 
 app.listen(3000, () => {
     console.log('Express server is running ....');
 })
-
-
-async function go() {
-    console.log(1);
-    const employees = await dal.get_all()
-    console.log(employees);
-    const employee = await dal.get_by_id(1)
-    console.log(employee);
-    // insert
-    // update
-    // delete
-}
-
-go()
